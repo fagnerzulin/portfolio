e99c83b7e0f19a2bf8658ec4f1b72595fee0ebf3
Fagner Zulin <fagnerzulin11@gmail.com>
2021-07-26 17:22:34 -0300

    Add AboutContainer and ImageContainer

--- src/components/MainContainer/MainContainer.js
+++ src/components/MainContainer/MainContainer.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import AboutContainer from '../AboutContainer/AboutContainer';
 import Header from '../Header/Header';
+import ImageContainer from '../ImageContainer/ImageContainer';
 import PrincipalContainer from './mainContainer.styled';
 
 export default function MainContainer() {
   return (
     <PrincipalContainer>
       <Header />
+      <AboutContainer />
+      <ImageContainer />
     </PrincipalContainer>
   );
 }
